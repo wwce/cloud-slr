f0964e5bbe93ae829e6832dc48b0b6235af85d81
jharris10 <jharris@paloaltonetworks.com>
2019-10-25 16:09:01 +0100

    Initial commit

---
+++ webserver-init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+apt-get update &&
+
+
+mkdir /usr/mirror &&
+
+PARAM_FILE=/usr/monitor/mirror.cfg
+echo "[DEFAULT]" > $PARAM_FILE
+echo "tmf=$1" >> $PARAM_FILE
+echo "region=$2" >> $PARAM_FILE
